d74da817b40e381678327d93d2d3510f72f7611f
Michal Podhradsky <mpodhradsky@galois.com>
2018-07-13 15:01:47 -0700

    First iteration of parsing incoming messages, including a trivial range check.

--- build.rs
+++ build.rs
@@ -1,23 +1,8 @@
-/*
-extern crate protobuf_codegen_pure;
-
-fn main() {
-    protobuf_codegen_pure::run(protobuf_codegen_pure::Args {
-        out_dir: "/home/michal/Workspace/CPS/pixhawk-proxy/protos",
-        input: &["/home/michal/Workspace/CPS/pixhawk-proxy/protos/mavlink.proto"],
-        includes: &["/home/michal/Workspace/CPS/pixhawk-proxy/protos"],
-        customize: protobuf_codegen_pure::Customize {
-            ..Default::default()
-        },
-    }).expect("protoc");
-}
-*/
 extern crate prost_build;
 
 fn main() {
     let mut config = prost_build::Config::new();
     config.type_attribute(".", "#[derive(Serialize)]");
-    //config.type_attribute(".", "#[serde(rename_all = \"camelCase\")]");
-    config.compile_protos(&["src/common.proto"],
-                                &["src/"]).unwrap();
+    config.compile_protos(&["protos/mavlink_common.proto"],
+                                &["protos/"]).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -7,95 +7,114 @@ extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 
-use std::io::Cursor;
+extern crate zmq;
+extern crate range_check;
+extern crate lmcp;
+
+// To check ranges for sending bytes to the autopilot (u32/i32 -> u8/i8 conversion)
+use range_check::Within;
 
 use prost::Message;
+use std::io::Cursor;
+
+use lmcp::afrl::cmasi::air_vehicle_state::AirVehicleState;
+use lmcp::afrl::cmasi::air_vehicle_state::AirVehicleStateT;
+use lmcp::afrl::cmasi::entity_state::EntityStateT;
 
 // Include the `items` module, which is generated from items.proto.
 pub mod mavlink_common {
     include!(concat!(env!("OUT_DIR"), "/mavlink.common.rs"));
 }
 
-fn alt_test() { 
-    let mut alt = mavlink_common::Altitude::default();
-    alt.time_usec = 53700512; 
-    alt.altitude_monotonic=488.23987;
-    alt.altitude_amsl = 488.01703;
-    alt.altitude_local=0.03985728;
-    alt.altitude_relative=0.012936121;
-    alt.altitude_terrain= std::f32::NAN;
-    alt.bottom_clearance = std::f32::NAN;
+pub struct PixhawkProxy {
+    current_air_vehicle_state: AirVehicleState,
+}
 
-    println!("original msg={:?}",alt);
+impl Default for PixhawkProxy {
+    fn default() -> PixhawkProxy {
+        PixhawkProxy {
+            current_air_vehicle_state: AirVehicleState::default(),
+        }
+    }
+}
 
-    let stream = serde_json::to_string(&alt).unwrap();
-    println!("json msg={}",stream);
-
-    /*
-    // Not supported right now
-    let new_alt: mavlink_common::Altitude = serde_json::from_str(&stream).unwrap();
-    */
-
-    let mut buf = Vec::new();
-    buf.reserve(alt.encoded_len());
-    alt.encode(&mut buf).unwrap();
-    println!("serialized len = {}", buf.len());
-
-    let new_msg = mavlink_common::Altitude::decode(&mut Cursor::new(buf));
-    println!("decoded msg = {:?}", new_msg);
+impl PixhawkProxy {
+    /// Parse an incoming Mavlink proto message and if it contains relevant data,
+    /// update the AirVehicleState struct
+    pub fn handle_proto_msg(&mut self, proto_msg: mavlink_common::MavlinkMessage) {
+        if let Some(msg) = proto_msg.msg_set {
+            use mavlink_common::mavlink_message::MsgSet::*;
+            match msg {
+                Heartbeat(data) => {
+                    // debug only
+                    println!("{:#?}", data);
+                    println!("{:#?}", self.current_air_vehicle_state);
+                }
+                VfrHud(data) => {
+                    *self.current_air_vehicle_state.airspeed_mut() = data.airspeed;
+                    // TODO: Check range?
+                    // [dependencies]
+                    // range_check = "0.1"
+                    // rust-range-check
+                    // use range_check::Within;
+                    // assert!(4123.is_within(3000..5000));
+                    //println!("Data heading = {}", data.heading);
+                    assert!(data.heading.is_within(0..360)); // heading is -1, so probably do some check around this
+                    *self.current_air_vehicle_state.course_mut() = data.heading as f32;
+                    *self.current_air_vehicle_state.groundspeed_mut() = data.groundspeed; // TODO: fix with GPS data
+                }
+                GlobalPositionInt(data) => {
+                    *self.current_air_vehicle_state.location.latitude_mut() = data.lat as f64 / 1e7;
+                    *self.current_air_vehicle_state.location.longitude_mut() =
+                        data.lon as f64 / 1e7;
+                    *self.current_air_vehicle_state.location.altitude_mut() =
+                        data.alt as f32 / 1000.0; // TODO: fix WGS84 to AMSL projection
+                                                  // *proxy.current_air_vehicle_state.location.altitude_type_mut() = AltitudeType::MSL ?
+                    *self.current_air_vehicle_state.vertical_speed_mut() = data.vz as f32 / -100.0; // TODO: check values
+                     /*
+                     Ground speed from NED: 
+                        let v_north_m_s: f32 = gps_data.v_north as f32 / 100.0;
+                        let v_east_m_s = gps_data.v_east as f32 / 100.0;
+                        let v_down_m_s = gps_data.v_down as f32 / 100.0;
+                        self.current_air_vehicle_state.course = v_east_m_s.atan2(v_north_m_s).to_degrees();
+                     */                }
+                HighresImu(data) => {
+                    *self.current_air_vehicle_state.udot_mut() = data.xacc;
+                    *self.current_air_vehicle_state.vdot_mut() = data.yacc;
+                    *self.current_air_vehicle_state.wdot_mut() = data.zacc;
+                }
+                Attitude(data) => {
+                    // TODO: yaw? Heading?
+                    *self.current_air_vehicle_state.heading_mut() = data.yaw.to_degrees() + 180.0;
+                    *self.current_air_vehicle_state.pitch_mut() = data.pitch.to_degrees();
+                    *self.current_air_vehicle_state.roll_mut() = data.roll.to_degrees();
+                    *self.current_air_vehicle_state.p_mut() = data.rollspeed.to_degrees();
+                    *self.current_air_vehicle_state.q_mut() = data.pitchspeed.to_degrees();
+                    *self.current_air_vehicle_state.r_mut() = data.yawspeed.to_degrees();
+                }
+                BatteryStatus(data) => {
+                    *self.current_air_vehicle_state.energy_available_mut() =
+                        data.battery_remaining as f32; // TODO: check for -1
+                    //*proxy.current_air_vehicle_state.actual_energy_rate_mut() = TODO: estimate the current energy rate from msg timestamp
+                }
+                _ => {}
+            }
+        }
+    }
 }
 
 fn main() {
-    //alt_test();
-    
-    let mut msg = mavlink_common::SysStatus::default();
-    msg.onboard_control_sensors_present = 32;
-    msg.onboard_control_sensors_enabled = 0;
-    msg.onboard_control_sensors_health = 2097184;
-    msg.load = 0;
-    msg.voltage_battery = 12150;
-    msg.current_battery = -100;
-    msg.drop_rate_comm = 0;
-    msg.errors_comm = 0;
-    msg.errors_count1 = 0;
-    msg.errors_count2 = 0;
-    msg.errors_count3 = 0;
-    msg.errors_count4 = 0;
-    msg.battery_remaining = 100;
-    println!("original msg={:?}\n",msg);
+    let context = zmq::Context::new();
+    let subscriber = context.socket(zmq::SUB).unwrap();
+    let filter = "";
+    assert!(subscriber.connect("tcp://localhost:5556").is_ok());
+    assert!(subscriber.set_subscribe(filter.as_bytes()).is_ok());
 
-    /*
-    let mut mavmsg = mavlink_common::MavlinkMessage::default();
-    mavmsg.msg_set = Some(mavlink_common::mavlink_message::MsgSet::Sysstatus(msg));
-    
-    let stream = serde_json::to_string(&mavmsg).unwrap();
-    println!("json msg={}\n",stream);
-    
-    let mut buf = Vec::new();
-    buf.reserve(mavmsg.encoded_len());
-    mavmsg.encode(&mut buf).unwrap();
-    println!("serialized len = {}\n", buf.len());
-    
-    let msg2: mavlink_common::MavlinkMessage = serde_json::from_str(&stream).unwrap();
-    println!("msg2={:?}\n",msg2);
-    */
+    let mut proxy = PixhawkProxy::default();
 
-    let v = serialize_msg(&msg);
-    println!("serialized len = {}", v.len());
-
-    let new_msg = deserialize_msg(&v).unwrap();
-    println!("decoded msg={:?}",new_msg);
-
-}
-
-pub fn serialize_msg(msg: &mavlink_common::SysStatus) -> Vec<u8> {
-    let mut buf = Vec::new();
-    buf.reserve(msg.encoded_len());
-    // Unwrap is safe, since we have reserved sufficient capacity in the vector.
-    msg.encode(&mut buf).unwrap();
-    buf
-}
-
-pub fn deserialize_msg(buf: &[u8]) -> Result<mavlink_common::SysStatus, prost::DecodeError> {
-    mavlink_common::SysStatus::decode(&mut Cursor::new(buf))
+    loop {
+        let stream = subscriber.recv_bytes(0).unwrap();
+        let msg = mavlink_common::MavlinkMessage::decode(&mut Cursor::new(stream)).unwrap();
+        proxy.handle_proto_msg(msg);
+    }
 }
