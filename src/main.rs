de3457e357c2276aef35e21a834d1a1ed8ab0c01
Michal Podhradsky <mpodhradsky@galois.com>
2018-07-17 17:14:40 -0700

    Better code structure, using channels for messaeg synchronization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+/// Main file for Pixhawk proxy
+///
+/// We have 4 threads (all use ZMQ for external message passing, and `sync::mpsc::channel` for internal message passing):
+/// 	LMCP TX: sends LMCP messages (mainly air_vehicle_state) to `LmcpObjectNetworkPublishPullBridge` when a new message is available
+///		LMCP RX: receives LMCP messages and processes them when appropriate (mostly waypoints)
+///		MAVLINK RX: receives Mavlink_protobuf messages and updates `air_vehicle_state` struct
+///		MAVLINK TX: sends Mavlink_protobuf messages that were created from incoming LMCP messages (such as mission and waypoint updates)
+///
 extern crate prost;
 #[macro_use]
 extern crate prost_derive;
@@ -7,174 +15,157 @@ extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 
-extern crate zmq;
-extern crate range_check;
 extern crate lmcp;
-
-// To check ranges for sending bytes to the autopilot (u32/i32 -> u8/i8 conversion)
-use range_check::Within;
+extern crate range_check;
+extern crate zmq;
 
 use prost::Message;
 use std::io::Cursor;
-use std::time::{Duration, Instant};
-
-use lmcp::afrl::cmasi::air_vehicle_state::AirVehicleState;
-use lmcp::afrl::cmasi::air_vehicle_state::AirVehicleStateT;
-use lmcp::afrl::cmasi::entity_state::EntityStateT;
-use lmcp::afrl::cmasi::altitude_type::AltitudeType;
+use std::sync::mpsc::{channel, Sender};
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 // Include the `items` module, which is generated from items.proto.
 pub mod mavlink_common {
     include!(concat!(env!("OUT_DIR"), "/mavlink.common.rs"));
 }
 
-const DEFAULT_KEEP_ALIVE_TIMEOUT: Duration = Duration::from_millis(10_000);
-
-/// Helper struct to keep energy data together
-struct BatteryStatus {
-    timestamp: Option<Instant>,
-    energy_available: Option<f32>,
-    energy_rate: Option<f32>, 
-}
-
-impl BatteryStatus {
-    pub fn new() -> BatteryStatus {
-        BatteryStatus {
-            timestamp: None,
-            energy_available: None,
-            energy_rate: None, 
-        }
-    } 
-    
-    /// Update internal values with new data
-    pub fn update(&mut self, timestamp: Instant, energy_available: f32) {
-        if let Some(last_ts) = self.timestamp {
-            let dt = timestamp.duration_since(last_ts);
-            if let Some(last_energy) = self.energy_available {
-                if last_energy == energy_available {
-                    // TODO: set to 0 if no change for a given time
-                    return; // no change
-                }
-                let dt = dt.as_secs() as f32 + dt.subsec_nanos() as f32 * 1e-9;
-                let e_rate = (energy_available - last_energy) / dt;
-                self.energy_rate = Some(e_rate);
-            }
-        }
-        self.timestamp = Some(timestamp);
-        self.energy_available = Some(energy_available);
-    }
-}
-
-pub struct PixhawkProxy {
-    last_heartbeat: Instant,
-    last_attitude_since_boot: Duration,
-    last_highres_imu_since_boot: Duration,
-    current_air_vehicle_state: AirVehicleState,
-    battery_status: BatteryStatus,
-}
-
-impl Default for PixhawkProxy {
-    fn default() -> PixhawkProxy {
-        PixhawkProxy {
-            last_heartbeat: Instant::now() - DEFAULT_KEEP_ALIVE_TIMEOUT,
-            last_attitude_since_boot: Duration::new(0,0),
-            last_highres_imu_since_boot: Duration::new(0,0),
-            current_air_vehicle_state: AirVehicleState::default(),
-            battery_status: BatteryStatus::new(),
-        }
-    }
-}
-
-impl PixhawkProxy {
-    /// Parse an incoming Mavlink proto message and if it contains relevant data,
-    /// update the AirVehicleState struct
-    pub fn handle_proto_msg(&mut self, proto_msg: mavlink_common::MavlinkMessage) {
-        if let Some(msg) = proto_msg.msg_set {
-            use mavlink_common::mavlink_message::MsgSet::*;
-            match msg {
-                Heartbeat(_data) => {
-                    // TODO: check if the heartbeat data are sane
-                    self.last_heartbeat = Instant::now();
-                    println!("{:#?}", self.current_air_vehicle_state);
-                }
-                VfrHud(data) => {
-                    // vehicle true airspeed [m/s]
-                    *self.current_air_vehicle_state.airspeed_mut() = data.airspeed;
-                    assert!(data.heading.is_within(-180..180));
-                    *self.current_air_vehicle_state.course_mut() = data.heading as f32;
-                    *self.current_air_vehicle_state.groundspeed_mut() = data.groundspeed;
-                }
-                GlobalPositionInt(data) => {
-                    // Course/Groundtrack angle of the entity referenced to true North [deg]
-                    // The perceived entity location. A valid EntityState must include Location (null not allowed)
-                    {
-                        *self.current_air_vehicle_state.location.latitude_mut() = data.lat as f64 / 1e7;
-                        *self.current_air_vehicle_state.location.longitude_mut() = data.lon as f64 / 1e7;
-                        // TODO: fix WGS84 to AMSL projection
-                        *self.current_air_vehicle_state.location.altitude_mut() = data.alt as f32 / 1000.0;
-                        // TODO: fix the altitude type (maybe use relative altitude?)
-                        *self.current_air_vehicle_state.location.altitude_type_mut() = AltitudeType::MSL;
-                    }
-                    // Vertical speed (positive upwards) of the vehicle in the inertial frame (rate of change of altitude) [m/s]
-                    *self.current_air_vehicle_state.vertical_speed_mut() = data.vz as f32 / -100.0;
-                }
-                HighresImu(data) => {
-                    // Timestamp (microseconds, synced to UNIX time or since system boot) [us]
-                    // TODO: check for staleness
-                    self.last_highres_imu_since_boot = Duration::from_micros(data.time_usec);
-                    // Acceleration in the body x-direction (postive out nose) [m/s^2]
-                    *self.current_air_vehicle_state.udot_mut() = data.xacc;
-                    // Acceleration in the body y-direction (postive out nose) [m/s^2]
-                    *self.current_air_vehicle_state.vdot_mut() = data.yacc;
-                    // Acceleration in the body z-direction (postive out nose) [m/s^2]
-                    *self.current_air_vehicle_state.wdot_mut() = data.zacc;
-                }
-                Attitude(data) => {
-                    // Timestamp (milliseconds since system boot) [ms]
-                    // TODO: check that it is monotonically increasing & check for staleness
-                    self.last_attitude_since_boot = Duration::from_millis(data.time_boot_ms.into());
-                    // Angle between true North and the projection of the body x-axis in the North-East plane. [deg]
-                    // TODO: Does pixhawk compensate for True vs Magnetic north? 
-                    *self.current_air_vehicle_state.heading_mut() = data.yaw.to_degrees();
-                    // Pitch of vehicle around body y-axis (positive upwards) [deg]
-                    *self.current_air_vehicle_state.pitch_mut() = data.pitch.to_degrees();
-                    // Roll angle of the vehicle around body x-axis (positive right wing down) [deg]
-                    *self.current_air_vehicle_state.roll_mut() = data.roll.to_degrees();
-                    // Roll-rate of vehicle (angular velocity around body x-axis).  Positive right-wing down. [deg/s]
-                    *self.current_air_vehicle_state.p_mut() = data.rollspeed.to_degrees();
-                    // pitch rate of the vehicle (angular velocity around body y-axis).  Positive nose-up. [deg/s]
-                    *self.current_air_vehicle_state.q_mut() = data.pitchspeed.to_degrees();
-                    // yaw rate of the vehicle (angular velocity around body z-axis).  Positive nose right. [deg/s] 
-                    *self.current_air_vehicle_state.r_mut() = data.yawspeed.to_degrees();
-                }
-                BatteryStatus(data) => {
-                    // TODO: check for -1
-                    self.battery_status.update(Instant::now(), data.battery_remaining as f32);
-                    if let Some(energy_available) = self.battery_status.energy_available {
-                        *self.current_air_vehicle_state.energy_available_mut() = energy_available;    
-                    }
-                    if let Some(energy_rate) = self.battery_status.energy_rate {
-                        *self.current_air_vehicle_state.actual_energy_rate_mut() = energy_rate;    
-                    }
-                }
-                _ => {}
-            }
-        }
-    }
-}
+mod pixhawk_proxy;
+use pixhawk_proxy::PixhawkProxy;
 
 fn main() {
+    // Default ZMQ context
     let context = zmq::Context::new();
-    let subscriber = context.socket(zmq::SUB).unwrap();
-    let filter = "";
-    assert!(subscriber.connect("tcp://localhost:5556").is_ok());
-    assert!(subscriber.set_subscribe(filter.as_bytes()).is_ok());
 
-    let mut proxy = PixhawkProxy::default();
+    // Define channels
+    let (lmcp_tx, lmcp_rx) = channel();
+    let (mavlink_tx, mavlink_rx) = channel();
 
-    loop {
-        let stream = subscriber.recv_bytes(0).unwrap();
-        let msg = mavlink_common::MavlinkMessage::decode(&mut Cursor::new(stream)).unwrap();
-        proxy.handle_proto_msg(msg);
+    // create proxy
+    let proxy = Arc::new(Mutex::new(PixhawkProxy::default()));
+
+    // keep thread handles
+    let mut handles = vec![];
+
+    // RX thread: receive and process Mavlink messages
+    handles.push(
+        thread::Builder::new()
+            .name("th_mavlink_rx".to_string())
+            .spawn({
+                let proxy = proxy.clone();
+                let send_to_lmcp = Sender::clone(&lmcp_tx);
+                let send_to_mavlink = Sender::clone(&mavlink_tx);
+                let subscriber = context.socket(zmq::SUB).unwrap();
+                let filter = "";
+                match subscriber.connect("tcp://127.0.0.1:44440") {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("Error: {}", e);
+                    }
+                }
+                assert!(subscriber.set_subscribe(filter.as_bytes()).is_ok());
+
+                move || loop {
+                    let stream = subscriber.recv_bytes(0).unwrap();
+                    let msg =
+                        mavlink_common::MavlinkMessage::decode(&mut Cursor::new(stream)).unwrap();
+                    match proxy.lock() {
+                        Ok(mut mav_proxy) => {
+                            mav_proxy.handle_proto_msg(msg);
+                        }
+                        Err(e) => {
+                            println!("Error locking proxy: {}", e);
+                        }
+                    }
+                    send_to_lmcp.send("test lmcp".as_bytes()).unwrap();
+                    send_to_mavlink.send("test mavlink".as_bytes()).unwrap();
+                }
+            }),
+    );
+
+    // RX thread: receive LMCP messages
+    handles.push(
+        thread::Builder::new()
+            .name("th_lmcp_rx".to_string())
+            .spawn({
+                let _proxy = proxy.clone();
+                let send_to_lmcp = Sender::clone(&lmcp_tx);
+                let send_to_mavlink = Sender::clone(&mavlink_tx);
+                let subscriber = context.socket(zmq::SUB).unwrap();
+                let filter = "";
+                match subscriber.connect("tcp://127.0.0.1:55560") {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("Error: {}", e);
+                    }
+                }
+                assert!(subscriber.set_subscribe(filter.as_bytes()).is_ok());
+
+                move || loop {
+                    let stream = subscriber.recv_bytes(0).unwrap();
+                    println!("Received {} bytes", stream.len());
+                    // TODO: parse LMCP message as needed
+                    send_to_lmcp.send("test lmcp".as_bytes()).unwrap();
+                    send_to_mavlink.send("test mavlink".as_bytes()).unwrap();
+                }
+            }),
+    );
+
+    // TX thread: Sending AirVehicle and other LMCP messages
+    handles.push(
+        thread::Builder::new()
+            .name("th_lmcp_tx".to_string())
+            .spawn({
+                let publisher = context.socket(zmq::PUB).unwrap();
+                match publisher.bind("tcp://127.0.0.1:55561") {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("Error: {}", e);
+                    }
+                }
+
+                move || loop {
+                    match lmcp_rx.recv() {
+                        Ok(data) => {
+                            // send data
+                            publisher.send(&data, 0).unwrap(); // send buffer with 0 flags
+                        }
+                        Err(e) => {
+                            println!("Error occured: {}", e);
+                        }
+                    }
+                }
+            }),
+    );
+
+    // TX thread: Sending Mavlink proto messages
+    handles.push(
+        thread::Builder::new()
+            .name("th_mavlink_tx".to_string())
+            .spawn({
+                let publisher = context.socket(zmq::PUB).unwrap();
+                match publisher.bind("tcp://127.0.0.1:44441") {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("Error: {}", e);
+                    }
+                }
+
+                move || loop {
+                    match mavlink_rx.recv() {
+                        Ok(data) => {
+                            // send data
+                            publisher.send(&data, 0).unwrap(); // send buffer with 0 flags
+                        }
+                        Err(e) => {
+                            println!("Error occured: {}", e);
+                        }
+                    }
+                }
+            }),
+    );
+
+    for handle in handles {
+        handle.unwrap().join().unwrap();
     }
 }
---
+++ src/pixhawk_proxy.rs
@@ -0,0 +1,153 @@
+// Pixhawk proxy module
+use std::time::{Duration, Instant};
+
+use lmcp::afrl::cmasi::air_vehicle_state::AirVehicleState;
+use lmcp::afrl::cmasi::air_vehicle_state::AirVehicleStateT;
+use lmcp::afrl::cmasi::altitude_type::AltitudeType;
+use lmcp::afrl::cmasi::entity_state::EntityStateT;
+//use lmcp::Message as LmcpMessage;
+
+// To check ranges for sending bytes to the autopilot (u32/i32 -> u8/i8 conversion)
+use range_check::Within;
+
+use super::mavlink_common;
+
+const DEFAULT_KEEP_ALIVE_TIMEOUT: Duration = Duration::from_millis(10_000);
+
+/// Helper struct to keep energy data together
+struct BatteryStatus {
+    timestamp: Option<Instant>,
+    energy_available: Option<f32>,
+    energy_rate: Option<f32>,
+}
+
+impl BatteryStatus {
+    pub fn new() -> BatteryStatus {
+        BatteryStatus {
+            timestamp: None,
+            energy_available: None,
+            energy_rate: None,
+        }
+    }
+
+    /// Update internal values with new data
+    pub fn update(&mut self, timestamp: Instant, energy_available: f32) {
+        if let Some(last_ts) = self.timestamp {
+            let dt = timestamp.duration_since(last_ts);
+            if let Some(last_energy) = self.energy_available {
+                if last_energy == energy_available {
+                    // TODO: set to 0 if no change for a given time
+                    return; // no change
+                }
+                let dt = dt.as_secs() as f32 + dt.subsec_nanos() as f32 * 1e-9;
+                let e_rate = (energy_available - last_energy) / dt;
+                self.energy_rate = Some(e_rate);
+            }
+        }
+        self.timestamp = Some(timestamp);
+        self.energy_available = Some(energy_available);
+    }
+}
+
+pub struct PixhawkProxy {
+    last_heartbeat: Instant,
+    last_attitude_since_boot: Duration,
+    last_highres_imu_since_boot: Duration,
+    pub current_air_vehicle_state: AirVehicleState,
+    battery_status: BatteryStatus,
+}
+
+impl Default for PixhawkProxy {
+    fn default() -> PixhawkProxy {
+        PixhawkProxy {
+            last_heartbeat: Instant::now() - DEFAULT_KEEP_ALIVE_TIMEOUT,
+            last_attitude_since_boot: Duration::new(0, 0),
+            last_highres_imu_since_boot: Duration::new(0, 0),
+            current_air_vehicle_state: AirVehicleState::default(),
+            battery_status: BatteryStatus::new(),
+        }
+    }
+}
+
+impl PixhawkProxy {
+    /// Parse an incoming Mavlink proto message and if it contains relevant data,
+    /// update the AirVehicleState struct
+    pub fn handle_proto_msg(&mut self, proto_msg: mavlink_common::MavlinkMessage) {
+        if let Some(msg) = proto_msg.msg_set {
+            use mavlink_common::mavlink_message::MsgSet::*;
+            match msg {
+                Heartbeat(_data) => {
+                    // TODO: check if the heartbeat data are sane
+                    self.last_heartbeat = Instant::now();
+                    println!("{:#?}", self.current_air_vehicle_state);
+                }
+                VfrHud(data) => {
+                    // vehicle true airspeed [m/s]
+                    *self.current_air_vehicle_state.airspeed_mut() = data.airspeed;
+                    assert!(data.heading.is_within(-180..180));
+                    *self.current_air_vehicle_state.course_mut() = data.heading as f32;
+                    *self.current_air_vehicle_state.groundspeed_mut() = data.groundspeed;
+                }
+                GlobalPositionInt(data) => {
+                    // Course/Groundtrack angle of the entity referenced to true North [deg]
+                    // The perceived entity location. A valid EntityState must include Location (null not allowed)
+                    {
+                        *self.current_air_vehicle_state.location.latitude_mut() =
+                            data.lat as f64 / 1e7;
+                        *self.current_air_vehicle_state.location.longitude_mut() =
+                            data.lon as f64 / 1e7;
+                        // TODO: fix WGS84 to AMSL projection
+                        *self.current_air_vehicle_state.location.altitude_mut() =
+                            data.alt as f32 / 1000.0;
+                        // TODO: fix the altitude type (maybe use relative altitude?)
+                        *self.current_air_vehicle_state.location.altitude_type_mut() =
+                            AltitudeType::MSL;
+                    }
+                    // Vertical speed (positive upwards) of the vehicle in the inertial frame (rate of change of altitude) [m/s]
+                    *self.current_air_vehicle_state.vertical_speed_mut() = data.vz as f32 / -100.0;
+                }
+                HighresImu(data) => {
+                    // Timestamp (microseconds, synced to UNIX time or since system boot) [us]
+                    // TODO: check for staleness
+                    self.last_highres_imu_since_boot = Duration::from_micros(data.time_usec);
+                    // Acceleration in the body x-direction (postive out nose) [m/s^2]
+                    *self.current_air_vehicle_state.udot_mut() = data.xacc;
+                    // Acceleration in the body y-direction (postive out nose) [m/s^2]
+                    *self.current_air_vehicle_state.vdot_mut() = data.yacc;
+                    // Acceleration in the body z-direction (postive out nose) [m/s^2]
+                    *self.current_air_vehicle_state.wdot_mut() = data.zacc;
+                }
+                Attitude(data) => {
+                    // Timestamp (milliseconds since system boot) [ms]
+                    // TODO: check that it is monotonically increasing & check for staleness
+                    self.last_attitude_since_boot = Duration::from_millis(data.time_boot_ms.into());
+                    // Angle between true North and the projection of the body x-axis in the North-East plane. [deg]
+                    // TODO: Does pixhawk compensate for True vs Magnetic north?
+                    *self.current_air_vehicle_state.heading_mut() = data.yaw.to_degrees();
+                    // Pitch of vehicle around body y-axis (positive upwards) [deg]
+                    *self.current_air_vehicle_state.pitch_mut() = data.pitch.to_degrees();
+                    // Roll angle of the vehicle around body x-axis (positive right wing down) [deg]
+                    *self.current_air_vehicle_state.roll_mut() = data.roll.to_degrees();
+                    // Roll-rate of vehicle (angular velocity around body x-axis).  Positive right-wing down. [deg/s]
+                    *self.current_air_vehicle_state.p_mut() = data.rollspeed.to_degrees();
+                    // pitch rate of the vehicle (angular velocity around body y-axis).  Positive nose-up. [deg/s]
+                    *self.current_air_vehicle_state.q_mut() = data.pitchspeed.to_degrees();
+                    // yaw rate of the vehicle (angular velocity around body z-axis).  Positive nose right. [deg/s]
+                    *self.current_air_vehicle_state.r_mut() = data.yawspeed.to_degrees();
+                }
+                BatteryStatus(data) => {
+                    // TODO: check for -1
+                    self.battery_status
+                        .update(Instant::now(), data.battery_remaining as f32);
+                    if let Some(energy_available) = self.battery_status.energy_available {
+                        *self.current_air_vehicle_state.energy_available_mut() = energy_available;
+                    }
+                    if let Some(energy_rate) = self.battery_status.energy_rate {
+                        *self.current_air_vehicle_state.actual_energy_rate_mut() = energy_rate;
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+}
