b0b45f40f289856cc31d4b710574d4c583fff395
Joseph Richardson <joe.richardson@tangramflex.com>
2019-10-15 13:37:44 -0400

    Parsed setup scripts minus a few

--- SETUP.sh
+++ SETUP.sh
@@ -3,3 +3,29 @@
 bash install_rust.sh
 
 ansible-playbook configure.yml
+
+cd ..
+bash clone_repos.sh
+
+echo "Build stuff"
+cd OpenUxAS && git checkout pixhawk && cd ..
+cd LmcpGen && ant jar && cd ..
+cd OpenAMASE/OpenAMASE && ant jar && cd ../..
+
+echo "Build UxAS - this is the problematic portion"
+echo "    Install prerequisites and generate lmcp"
+cd OpenUxAS
+
+bash install_prerequisites.sh
+
+bash RunLmcpGen.sh
+
+echo "    Build OpenUxAS"
+./rm-external
+./prepare
+meson build --buildtype=release
+ninja -C build all
+cd ../
+
+echo "Done building stuff"
+
---
+++ clone_repos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+git clone https://github.com/GaloisInc/lmcp_sentinelizer.git
+git clone https://github.com/GaloisInc/uxas_attribute_message.git
+git clone https://github.com/GaloisInc/mavlink2protobuf_service.git
+# git clone https://github.com/GaloisInc/pixhawk-proxy.git <- this already exists
+
+echo "Clone PX4 Firmware"
+git clone -b uxas_master https://github.com/GaloisInc/Firmware.git
+
+echo "Download QGroundControl"
+wget https://s3-us-west-2.amazonaws.com/qgroundcontrol/latest/QGroundControl.AppImage && chmod +x ./QGroundControl.AppImage
+
+echo "Clone UxAS (Galois fork)"
+git clone https://github.com/GaloisInc/OpenUxAS
+git clone https://github.com/afrl-rq/LmcpGen.git
+
+echo "Clone and build OpenAMASE"
+git clone https://github.com/afrl-rq/OpenAMASE.git
+
+echo "Get UxAS prerequisities"
+cd OpenUxAS && git checkout pixhawk
